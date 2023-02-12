607f7b34316d6a3eb6db21eb8904072cb0f2c9d2
aleksandr burakov <alexb@aleksandrs-MacBook-Pro.local>
2023-02-12 23:25:24 +0100

    Initial

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a62e419f-6c58-4ac8-9550-b84d208ffce2';
+export const MODAL_CLOSE_SEC = 2.5;
+
+//
+//
